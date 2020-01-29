7f11ccab2c33b25b98211072e388035982ade2bd
Étienne Lévesque <etienne.levesque16@gmail.com>
2020-01-29 13:36:38 -0500

    Day 1, part 2

--- lib/solvers/Day1.ex
+++ lib/solvers/Day1.ex
@@ -3,16 +3,29 @@ defmodule Advent.Solvers.Day1 do
 
   @impl true
   def solve(part, input) do
+    recursive = part == 2
+
     input
     |> String.split()
     |> Enum.map(&String.to_integer/1)
-    |> Enum.map(&calculate_fuel/1)
+    |> Enum.map(fn mass -> calculate_fuel(mass, recursive) end)
     |> Enum.sum()
   end
 
-  defp calculate_fuel(mass) do
-    mass
-    |> div(3)
-    |> Kernel.-(2)
+  defp calculate_fuel(mass, recursive) do
+    fuel =
+      mass
+      |> div(3)
+      |> Kernel.-(2)
+
+    if fuel <= 0 do
+      0
+    else
+      if recursive do
+        fuel + calculate_fuel(fuel, true)
+      else
+        fuel
+      end
+    end
   end
 end
