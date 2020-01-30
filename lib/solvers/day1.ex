d6db5339e33e20ed0bd4d18ab11387103530aeaf
Étienne Lévesque <etienne.levesque16@gmail.com>
2020-01-29 20:35:42 -0500

    Clean day 1

--- lib/solvers/day1.ex
+++ lib/solvers/day1.ex
@@ -12,20 +12,19 @@ defmodule Advent.Solvers.Day1 do
     |> Enum.sum()
   end
 
-  defp calculate_fuel(mass, recursive) do
-    fuel =
-      mass
-      |> div(3)
-      |> Kernel.-(2)
+  defp calculate_fuel(mass, false) do
+    mass
+    |> div(3)
+    |> Kernel.-(2)
+  end
+
+  defp calculate_fuel(mass, true) do
+    fuel = calculate_fuel(mass, false)
 
     if fuel <= 0 do
       0
     else
-      if recursive do
-        fuel + calculate_fuel(fuel, true)
-      else
-        fuel
-      end
+      fuel + calculate_fuel(fuel, true)
     end
   end
 end
