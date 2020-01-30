24604ccd9b57bd8f3632cb6af8487523a287b4ac
Étienne Lévesque <etienne.levesque16@gmail.com>
2020-01-29 21:35:36 -0500

    Day 2, part 2

--- lib/solvers.ex
+++ lib/solvers.ex
@@ -1,4 +1,4 @@
 defmodule Advent.Solvers do
   @doc "Defines the function executed when running the day's solution"
-  @callback solve(part :: Integer, input :: binary()) :: any()
+  @callback solve(part :: 1 | 2, input :: binary()) :: any()
 end
--- lib/solvers/day2.ex
+++ lib/solvers/day2.ex
@@ -2,44 +2,65 @@ defmodule Advent.Solvers.Day2 do
   @behaviour Advent.Solvers
 
   @impl true
-  def solve(_part, input) do
+  def solve(1, input) do
     memory =
       input
       |> String.split(",")
       |> Enum.map(&String.to_integer/1)
-      |> List.replace_at(1, 12)
-      |> List.replace_at(2, 2)
+      |> initialize(12, 2)
 
     run(memory, 0)
   end
 
-  defp run(memory, pc) do
-    Enum.at(memory, pc)
-    |> decode(memory, pc)
+  @impl true
+  def solve(2, input) do
+    memory =
+      input
+      |> String.split(",")
+      |> Enum.map(&String.to_integer/1)
+
+    {noun, verb} =
+      for noun <- 0..99, verb <- 0..99 do
+        {noun, verb}
+      end
+      |> Enum.find(fn {noun, verb} -> initialize(memory, noun, verb) |> run(0) == 19_690_720 end)
+
+    100 * noun + verb
   end
 
-  defp decode(99, memory, _pc) do
+  defp initialize(memory, noun, verb) do
+    memory
+    |> List.replace_at(1, noun)
+    |> List.replace_at(2, verb)
+  end
+
+  defp run(memory, ip) do
+    Enum.at(memory, ip)
+    |> decode(memory, ip)
+  end
+
+  defp decode(99, memory, _ip) do
     Enum.at(memory, 0)
   end
 
-  defp decode(1, memory, pc) do
-    execute_op(3, memory, pc, &Kernel.+/2)
+  defp decode(1, memory, ip) do
+    execute_op(3, memory, ip, &Kernel.+/2)
   end
 
-  defp decode(2, memory, pc) do
-    execute_op(3, memory, pc, &Kernel.*/2)
+  defp decode(2, memory, ip) do
+    execute_op(3, memory, ip, &Kernel.*/2)
   end
 
-  defp decode(opcode, _memory, _pc) do
-    "Invalid opcode #{opcode}."
+  defp decode(opcode, _memory, _ip) do
+    "Invalid oipode #{opcode}."
   end
 
-  defp execute_op(3, memory, pc, fun) do
-    arg1 = Enum.at(memory, pc + 1)
-    arg2 = Enum.at(memory, pc + 2)
-    arg3 = Enum.at(memory, pc + 3)
+  defp execute_op(3, memory, ip, fun) do
+    arg1 = Enum.at(memory, ip + 1)
+    arg2 = Enum.at(memory, ip + 2)
+    arg3 = Enum.at(memory, ip + 3)
 
     List.replace_at(memory, arg3, fun.(Enum.at(memory, arg1), Enum.at(memory, arg2)))
-    |> run(pc + 4)
+    |> run(ip + 4)
   end
 end
